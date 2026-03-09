e91f05ab924941eaf1d7ba27bd40e867e100df62
Ahmad61-6 <ahmadmizan210@gmail.com>
2026-03-10 00:33:50 +0600

    text view and image comp learned

--- learnings/shelfie_app/app/index.jsx
+++ learnings/shelfie_app/app/index.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
-import { Text, StyleSheet, View } from 'react-native'
-
+import { Text, StyleSheet, View, Image } from 'react-native'
+import Logo from '../assets/img/logo_light.png'
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+        <Image source = {Logo} style = {styles.img}></Image>
+      <Text style = {[styles.title, {color:'purple'}] }>Number 1</Text>
+        <Text style ={{marginBottom: 20}}>Reading list app</Text>
+
+
+
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    title:{
+        fontWeight:'bold',
+        fontSize:20,
+    },
+    
+    img: {
+        marginVertical: 20,
+    }
+})
