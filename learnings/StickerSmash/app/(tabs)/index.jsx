06fd1e455bfa3ff7f3d3d387749044ebebd8ae59
Ahmad61-6 <ahmadmizan210@gmail.com>
2026-03-19 02:14:25 +0600

    completed the expo default project

--- learnings/StickerSmash/app/(tabs)/index.jsx
+++ learnings/StickerSmash/app/(tabs)/index.jsx
@@ -1,7 +1,9 @@
+import domtoimage from "dom-to-image";
 import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
-import { useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { Platform, StyleSheet, View } from "react-native";
+import { captureRef } from "react-native-view-shot";
 import CircleButton from "../../components/CircleButton";
 import EmojiList from "../../components/EmojiList";
 import EmojiPicker from "../../components/EmojiPicker";
@@ -14,6 +16,7 @@ const PlaceHolderImage = require("../../assets/images/background-image.png");
 
 export default function Index() {
   const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
+  const imageRef = useRef(null);
 
   const [selectedImage, setSelectedImage] = useState(undefined);
   const [showAppOptions, setShowAppOptions] = useState(false);
@@ -52,15 +55,48 @@ export default function Index() {
     setIsModalVisible(true);
   };
 
-  const onSaveImageAsync = async () => {};
+  const onSaveImageAsync = async () => {
+    if (Platform.OS !== "web") {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert("Saved!");
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement("a");
+        link.download = "sticker-smash.jpeg";
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={selectedImage || PlaceHolderImage} />
-        {pickedEmoji && (
-          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
-        )}
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer imgSource={selectedImage || PlaceHolderImage} />
+          {pickedEmoji && (
+            <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+          )}
+        </View>
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
